5b534f5036c9149ae8d7c883c48b3069e085fadb
constanzajazme <constanza.jazmeh@utem.cl>
2019-05-10 19:55:46 -0400

    tarea 2

--- tarea3/tarea3.cpp
+++ tarea3/tarea3.cpp
@@ -34,7 +34,7 @@ int* create_sub_vec(int begin, int end, int* origin){
 /**Función para obtener el promedio de un subvector**/
 
 float get_sub_prom(int begin, int end, int* sub_vec){ /* Se ingresa a la función: el inicio del sub_vec, el fin, el sub_vec y el vector con datos*/
-  float sub_prom=0;  /* variable en donde se almacenará el promedio del sub vector*/
+  float sub_prom=0.0;  /* variable en donde se almacenará el promedio del sub vector*/
   int aux=0;    /* Espacios que avanzara en el arreglo*/
   float sum=0.0;    /* Suma de los elementos del vector*/
 
@@ -48,15 +48,7 @@ float get_sub_prom(int begin, int end, int* sub_vec){ /* Se ingresa a la funció
 
 }
 
-
-void print_all_prom(int p, float* all_prom){
-   printf("#########################ESTO ES UNA PRUEBA####################\n");
-    for (int i = 0; i < p; ++i)
-    {
-        printf("%f, ", *(all_prom+i));
-    }
-    printf("#############FIN PRUEBA################\n");
-}
+/*Función que imprime todos los valores del arreglo*/
 
 void print(int my_rank, int comm_sz, int n_over_p, int* sub_vec){
     printf("Process %d out of %d received sub_vecotr: [ ", my_rank, comm_sz);
@@ -72,15 +64,13 @@ int main(void){
   int mi_rango; /* rango del proceso    */
   int p; /* numero de procesos   */
   int fuente; /* rango del que envia  */
-  int dest; /* rango del que recibe */
   int tag = 0; /* etiqueta del mensaje */
-  float sub_prom;
+  float sub_prom=0;
 
   int div_conjunto; /* n sobre p, indica en cuantos datos contiene un conjunto ideal (simetrico con los demás)*/
   int vec[100000]; /* Declaración del arreglo*/
   int* sub_vec = NULL;
-  float info_clave[3];
-
+  float prom=0.0;
 
   MPI_Init(NULL, NULL);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
@@ -89,28 +79,33 @@ int main(void){
 
   div_conjunto = 100000/p;
 //  sub_prom = (float*)malloc(size * sizeof(int));
-  printf("Process %d calcula n_over_p = %d\n", mi_rango, div_conjunto);
+
 
     if (mi_rango != 0) {  /*En esta parte se tiene el arreglo con datos*/
         sub_vec = (int*)malloc(div_conjunto * sizeof(int));
-        MPI_Recv(sub_vec, div_conjunto, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        print(mi_rango, p, div_conjunto, sub_vec);
+        MPI_Recv(&sub_prom, div_conjunto, MPI_FLOAT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Arreglo listo! \n");
+        prom=prom+sub_prom;
+
+        printf("El promedio de todos los datos del arreglo es de %f\n",prom );
+        //print(mi_rango, p, div_conjunto, sub_vec);
 
 
     } else {    /* Se rellena el arreglo*/
 
-        printf("Distribuindo dados\n");
+        printf("Rellenando arreglo con 100.000\n");
         for (fuente = 1; fuente < p; fuente++) {
             sub_vec = create_sub_vec(fuente*div_conjunto, (fuente*div_conjunto)+div_conjunto, vec);
             sub_prom=get_sub_prom(fuente*div_conjunto, (fuente*div_conjunto)+div_conjunto, sub_vec);
-            info_clave[0]=sub_vec;
-            info_clave[1]=sub_prom;
-            MPI_Send(sub_vec, div_conjunto, MPI_INT, fuente, tag, MPI_COMM_WORLD);
+
+
+            MPI_Send(&sub_prom, div_conjunto, MPI_FLOAT, fuente, tag, MPI_COMM_WORLD);
         }
-        printf("Fim da distribuicao de dados\n");
+
 
         sub_vec = create_sub_vec(0, div_conjunto, vec); //llena el primer conjunto de datos
         sub_prom=get_sub_prom(0,div_conjunto, sub_vec);
+
 
         //print(mi_rango, p, div_conjunto, sub_vec);
     }
