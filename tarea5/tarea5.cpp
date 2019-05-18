f4a52ff3ed641130deeeb2fa87cd0cfe78c90a30
maurigzz <mauricioalejandrogonzalezb@gmail.com>
2019-05-17 22:31:18 -0400

    Creación Struct
    Se crean dos struct iniciales para guardar los datos de los archivos .xlsx

---
+++ tarea5/tarea5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+struct docente{
+    int id_docente;
+    char nombre[100];
+    char apellido[100];
+};
+
+struct asignaturas{
+    int id_curso;
+    char nombre[100];
+    int id_docente;
+};
+
+int main(){
+
+    return 0;
+
+}
