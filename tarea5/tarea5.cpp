d977f5d7fa2057b4f9c70407c208596dfd0ea7fd
maurigzz <mauricioalejandrogonzalezb@gmail.com>
2019-05-18 00:02:36 -0400

    Actualizacion
    Metodo para verificar la cantidad de argumentos.

--- tarea5/tarea5.cpp
+++ tarea5/tarea5.cpp
@@ -15,7 +15,28 @@ struct asignaturas{
     int id_docente;
 };
 
-int main(){
+/* void llenar_docente(){
+}*/
+
+/* void llenar_asignaturas(){
+
+}*/
+
+/* Verifica que la cantidad de argumentos ingresados coincida con la cantidad requerida */
+int verificar_argc(int argc, int cantidad){
+    int cantidad_argc = argc - 1;
+
+    /* Si la cantidad de argumentos es igual a la cantidad requerida, retorna un 1 */
+    if (cantidad_argc = cantidad){
+        return 1;
+    }
+    else /* En caso contrario retorna un 0 */
+    {
+        return 0;
+    }
+}
+
+int main(int argc, char *argv[]){
 
     return 0;
 
